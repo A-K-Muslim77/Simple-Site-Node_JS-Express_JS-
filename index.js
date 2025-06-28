72d9587aedf0014dfde285dda46bc903529ea92c
A-K-Muslim77 <abdul177kayum@gmail.com>
2025-06-28 15:46:36 +0600

    Added Response status code and Client IP address

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const fs = require('fs'); // Added for file logging
 
 //  Logging middleware
 app.use((req, res, next) => {
-    const logEntry = `[${new Date().toLocaleString()}] ${req.method} ${req.url}\n`;
+  const start = new Date();
+
+  // When response finishes, log details
+  res.on('finish', () => {
+    const timestamp = start.toLocaleString();
+    const method = req.method;
+    const url = req.url;
+    const status = res.statusCode;
+    const ip = req.ip || req.connection.remoteAddress;
+
+    const logEntry = `[${timestamp}] ${ip} ${method} ${url} ${status}\n`;
+
     fs.appendFile(path.join(__dirname, 'log.txt'), logEntry, (err) => {
-        if (err) {
-            console.error('Failed to write log:', err);
-        }
+      if (err) {
+        console.error('Failed to write log:', err);
+      }
     });
-    next();
+  });
+
+  next();
 });
+
 
 
 
